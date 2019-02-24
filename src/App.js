bd195663ab659eecbb05c68a7f020af051190ab8
Beshoy.R.Mansour <beshoy.r.mansour@gmail.com>
2019-02-24 13:01:37 +0200

    new Project set up

--- src/App.js
+++ src/App.js
@@ -1,27 +1,12 @@
 import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-
-import Home from "./components/home/home";
-import PostDetails from "./components/posts/postDetails";
-import Navbar from "./components/navbar/navbar";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <BrowserRouter>
-          <React.Fragment>
-          <Navbar />
-            <div className="container">
-              <Switch>
-                <Route exact path="/" component={Home} />
-                <Route path="/posts/:post_id" component={PostDetails} />
-              </Switch>
-            </div>
-          </React.Fragment>
-        </BrowserRouter>
+        <h1>Hello</h1>
       </div>
     );
   }
--- src/actions/postActions.js
+++
@@ -1,6 +0,0 @@
-export const deletePost = id => {
-  return {
-    type: "DLETE_POST",
-    id
-  };
-};
